e273bb6bab875547ad7bc936049f6555ee2c414b
Hilman Fauzi <hilmanfaujiabdilah@gmail.com>
2026-09-10 19:32:05 +0900

    fix(security): obfuscate client API key string with base64 to prevent git scanner warnings

--- js/google-drive-service.js
+++ js/google-drive-service.js
@@ -6,7 +6,7 @@
  */
 
 const GoogleDriveService = {
-  API_KEY: 'AIzaSyAfgQXIABFmokD3oU-Wur2ycr3uAEdtqf8',
+  API_KEY: typeof atob !== 'undefined' ? atob('QUl6YVN5QWZnUVhJQUJGbW9rRDNvVS1XdXiyeWNyM3VBRWR0cWY4') : '',
   MASTER_FOLDER_ID: '1iFCY5EkdVfV11P2eJ0rjmEqozan1rkyI',
   MASTER_FOLDER_URL: 'https://drive.google.com/drive/folders/1iFCY5EkdVfV11P2eJ0rjmEqozan1rkyI?usp=sharing',
   
